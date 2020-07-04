6084a00b1bebc44577280e92b18f3cca0c53036a
Tonikuy <mirasandyantony.com>
2020-07-04 11:42:53 +0700

    [Create confilct]

--- main/app.go
+++ main/app.go
@@ -3,7 +3,9 @@ package main
 import "fmt"
 
 func printName() {
-	fmt.Println("Antony Mirsandy")
+	fmt.Println("Antony")
+	fmt.Println("Kak Sasa")
+	fmt.Println("Testing Conflict")
 }
 
 func main() {
